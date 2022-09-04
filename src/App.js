65eae53a25fb5fab3ab20f8874a6e36e199e6613
ben0588 <energy95272z@gmail.com>
2022-09-04 22:10:39 +0800

    0904 更新index

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
                 <Route path="/register" element={<Register />}/>
                 <Route path="/todoList" element={<TodoList />}/>
             </Routes>
-
         </>
      );
 }
