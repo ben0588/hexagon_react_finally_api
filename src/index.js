7d0d5989b29cb07d25955769fb1ceb36cd5f2b83
ben0588 <energy95272z@gmail.com>
2022-11-16 09:57:14 +0800

    更新 google 分析

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter,HashRouter } from 'react-router-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import './index.css'
 
+// 引用 google分析
+import ReactGA from 'react-ga'
+ReactGA.initialize('G-F4N0NX0YNJ')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+    <HashRouter>
+        <App />
+    </HashRouter>
+)
